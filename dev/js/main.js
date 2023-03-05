05459f22095264293451c67681789ed22ce8bc6a
Владислав Белов <belov_vlad@list.ru>
2023-03-05 19:05:24 +0300

    05

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,2 +1,2 @@
-const fn=n=>{console.log(n)};fn("some string");
+const fn=n=>{console.log(n)};function getCombinations(n,e){let o=[];return function e(t,i,s){if(0!==i){if(!(i<0))for(let o=s;o<n.length;o++)t.includes(n[o])||(t.push(n[o]),e(t,i-n[o],o+1),t.pop())}else o.push([...t])}([],e,0),Array.from(new Set(o.map(JSON.stringify)),JSON.parse)}
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-fn('some string');
+function getCombinations(arr, n) {
+  let result = [];
+  function backtrack(tempList, remaining, start) {
+    if (remaining === 0) {
+      result.push([...tempList]);
+      return;
+    }
+    if (remaining < 0) {
+      return;
+    }
+    for (let i = start; i < arr.length; i++) {
+      if (tempList.includes(arr[i])) {
+        continue;
+      }
+      tempList.push(arr[i]);
+      backtrack(tempList, remaining - arr[i], i + 1);
+      tempList.pop();
+    }
+  }
+  backtrack([], n, 0);
+  return Array.from(new Set(result.map(JSON.stringify)), JSON.parse);
+}
