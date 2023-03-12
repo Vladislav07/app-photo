f2bf12052187f4f6c1b1977bd21f6acbba58aaa2
Владислав Белов <belov_vlad@list.ru>
2023-03-12 21:25:39 +0300

    12

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+let burger = document.querySelector(".burger");
+let menu = document.querySelector(".header__nav");
+let menuLinks=document.querySelectorAll(".nav__link");
+
+burger.addEventListener("click", function () {
+  burger.classList.toggle("burger--active");
+  menu.classList.toggle("header__nav--active");
+  document.body.classList.toggle('stop-scroll');
+});
+
+menuLinks.forEach(function(el){
+  el.addEventListener("click",
+  function(){
+    burger.classList.remove("burger--active");
+    menu.classList.remove("header__nav--active");
+    document.body.classList.remove('stop-scroll');
+  })
+})
+
+let btnSearh = document.querySelector(".search");
+let fieldSearch=document.querySelector(".header__search");
+let btnCloseSearch=document.querySelector(".header__close-search")
+
+btnSearh.addEventListener("click",
+function () {
+    alert("jjjj");
+  fieldSearch.classList.add("header__search--active")
+})
+
+btnCloseSearch.addEventListener("click",
+function () {
+  fieldSearch.classList.remove("header__search--active")
+})
